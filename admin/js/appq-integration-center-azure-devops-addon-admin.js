38572de374f6bae620794c3dff3f3f37e3e2e72e
Davide Bizzi <davide.bizzi@app-quality.com>
2019-10-22 14:39:24 +0200

    feat: Add spinner when uploading bug

--- admin/js/appq-integration-center-azure-devops-addon-admin.js
+++ admin/js/appq-integration-center-azure-devops-addon-admin.js
@@ -5,6 +5,8 @@
 		$('#bugs-tabs-content .fa.fa-upload').not(".text-secondary").click(function() {
 			var cp_id = $('#cp_id').val()
 			var bug_id = $(this).data('bug-id')
+			var button = $(this)
+			button.removeClass('fa-upload').addClass('fa-spinner fa-spin text-secondary')
 			jQuery.ajax({
 				type: "post",
 				dataType: "json",
@@ -15,6 +17,8 @@
 					'bug_id': bug_id
 				},
 				success: function(msg) {
+					button.removeClass('fa-spinner fa-spin').addClass('fa-upload')
+					button.closest('tr').find('td.is_uploaded').append('<span class="fa fa-check"></span>')
 					console.log(msg);
 				}
 			});
