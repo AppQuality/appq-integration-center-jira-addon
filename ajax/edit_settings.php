cb24b03c866d7ff35c2f3f675555226400fc4313
Davide Bizzi <davide.bizzi@app-quality.com>
2019-10-23 10:38:00 +0200

    fix: Allow setting save without bugtracker set

--- ajax/edit_settings.php
+++ ajax/edit_settings.php
@@ -8,12 +8,21 @@ function appq_azure_devops_edit_settings()
 	$apikey = array_key_exists('azure_devops_apikey', $_POST) ? $_POST['azure_devops_apikey'] : '';
 	$field_mapping = array_key_exists('field_mapping', $_POST) ? $_POST['field_mapping'] : new stdClass();
 	$field_mapping = stripslashes(json_encode($field_mapping));
-	
-	$wpdb->update($wpdb->prefix .'appq_integration_center_config',array(
+
+	$has_value = intval($wpdb->get_var(
+		$wpdb->prepare('SELECT COUNT(*) FROM ' .$wpdb->prefix .'appq_integration_center_config WHERE integration = "azure-devops" AND campaign_id = %d', $cp_id)
+	));
+	if ($has_value === 0) {
+		$wpdb->insert($wpdb->prefix .'appq_integration_center_config', array(
+			'integration' => 'azure-devops',
+			'campaign_id' => $cp_id,
+		));
+	}
+	$wpdb->update($wpdb->prefix .'appq_integration_center_config', array(
 		'endpoint' => $endpoint,
 		'apikey' => $apikey,
 		'field_mapping' => $field_mapping,
-	),array(
+	), array(
 		'integration' => 'azure-devops',
 		'campaign_id' => $cp_id,
 	));
