cb93af278abd819ed0af2c43e9ecc55408d2cc15
Luca Cannarozzo <cannarozzoluca@live.it>
2021-02-18 12:46:16 +0100

    Fix: fatal error

--- admin/partials/appq-integration-center-jira-addon-admin-settings/field-mapping.php
+++ admin/partials/appq-integration-center-jira-addon-admin-settings/field-mapping.php
@@ -7,44 +7,54 @@ foreach ($api->basic_configuration as $key => $value) {
 }
 ?>
 
-<div class="row">
-	<div class="col-6"><?php printf('<h4 class="title py-3">%s</h4>', __('Field mapping', $this->plugin_name)); ?></div>
-	<div class="col-6 text-right actions mt-2">
-		<button type="button" class="btn btn-secondary mr-1" data-toggle="modal" data-target="#get_from_bug"><?php _e('Get mapping from bug', $this->plugin_name); ?></button>
-		<button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#add_mapping_field_modal"><?php _e('New mapping field', $this->plugin_name); ?></button>
-	</div>
-</div>
-<div class="row mb-2">
-    <div class="col-2">
-        <?php printf('<small><strong>%s</strong></small>', __('Name', $this->plugin_name)); ?>
+    <div class="row">
+        <div class="col-6"><?php printf('<h4 class="title py-3">%s</h4>', __('Field mapping', $this->plugin_name)); ?></div>
+        <div class="col-6 text-right actions mt-2">
+            <button type="button" class="btn btn-secondary mr-1" data-toggle="modal" data-target="#get_from_bug"><?php _e('Get mapping from bug', $this->plugin_name); ?></button>
+            <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#add_mapping_field_modal"><?php _e('New mapping field', $this->plugin_name); ?></button>
+        </div>
     </div>
-    <div class="col-4">
-        <?php printf('<small><strong>%s</strong></small>', __('Content', $this->plugin_name)); ?>
+    <div class="row mb-2">
+        <div class="col-2">
+			<?php printf('<small><strong>%s</strong></small>', __('Name', $this->plugin_name)); ?>
+        </div>
+        <div class="col-4">
+			<?php printf('<small><strong>%s</strong></small>', __('Content', $this->plugin_name)); ?>
+        </div>
+        <div class="col-2 text-center">
+			<?php printf('<small><strong>%s</strong></small>', __('Needs sanitizing', $this->plugin_name)); ?>
+        </div>
+        <div class="col-2 text-center">
+			<?php printf('<small><strong>%s</strong></small>', __('Contains JSON', $this->plugin_name)); ?>
+        </div>
     </div>
-    <div class="col-2 text-center">
-        <?php printf('<small><strong>%s</strong></small>', __('Needs sanitizing', $this->plugin_name)); ?>
+    <div class="fields-list">
+		<?php foreach ($field_mapping as $key => $item): ?>
+            <div class="row mb-2" data-row="<?= $key ?>">
+                <div class="col-2"><?= $key ?></div>
+                <div class="col-4"><?= array_key_exists('value', $item) ? nl2br($item['value']) : '' ?></div>
+                <div class="col-2 text-center">
+                    <input type="checkbox" style="cursor: unset;" disabled <?= array_key_exists('sanitize', $item) && $item['sanitize'] == 'on' ? ' checked="checked"' : '' ?>>
+                </div>
+                <div class="col-2 text-center">
+                    <input type="checkbox" style="cursor: unset;" disabled <?= array_key_exists('is_json', $item) && $item['is_json'] == 'on' ? ' checked="checked"' : '' ?>>
+                </div>
+                <div class="col-2 text-right actions">
+                    <button data-toggle="modal" data-target="#add_mapping_field_modal" type="button" class="btn btn-secondary mr-1 edit-mapping-field"
+                            data-key="<?= esc_attr($key); ?>"
+                            data-content="<?= (isset($item['value']) ? esc_attr($item['value']) : '') ?>"
+                            data-sanitize="<?= (isset($item['sanitize']) ? esc_attr($item['sanitize']) : '') ?>"
+                            data-json="<?= (isset($item['is_json']) ? esc_attr($item['is_json']) : '') ?>">
+                        <i class="fa fa-pencil"></i>
+                    </button>
+                    <button data-toggle="modal" data-target="#delete_mapping_field_modal" type="button" class="btn btn-secondary delete-mapping-field"
+                            data-key="<?= esc_attr($key) ?>">
+                        <i class="fa fa-trash"></i>
+                    </button>
+                </div>
+            </div>
+		<?php endforeach; ?>
     </div>
-    <div class="col-2 text-center">
-        <?php printf('<small><strong>%s</strong></small>', __('Contains JSON', $this->plugin_name)); ?>
-    </div>
-</div>
-<div class="fields-list">
-<?php foreach ($field_mapping as $key => $item){ ?>
-    <div class="row mb-2" data-row="<?= $key ?>">
-        <?php
-        printf(
-            '<div class="col-2">%s</div><div class="col-4">%s</div><div class="col-2 text-center"><input type="checkbox" style="cursor: unset;" disabled%s></div><div class="col-2 text-center"><input type="checkbox" style="cursor: unset;" disabled%s></div><div class="col-2 text-right actions">%s</div>',
-            $key,
-			array_key_exists('value', $item) ? nl2br($item['value']) : '',
-			array_key_exists('sanitize', $item) && $item['sanitize'] == 'on' ? ' checked="checked"' : '',
-			array_key_exists('is_json', $item) && $item['is_json'] == 'on' ? ' checked="checked"' : '',
-			'<button data-toggle="modal" data-target="#add_mapping_field_modal" type="button" class="btn btn-secondary mr-1 edit-mapping-field" data-key="'.esc_attr($key).'" data-content="'.(isset($item['value']) ? esc_attr($item['value']) : '').'" data-sanitize="'.(isset($item['sanitize']) ? esc_attr($item['sanitize']) : '').'" data-json="'.(isset($item['is_json']) ? esc_attr($item['is_json']) : '').'"><i class="fa fa-pencil"></i></button>
-			<button data-toggle="modal" data-target="#delete_mapping_field_modal" type="button" class="btn btn-secondary delete-mapping-field" data-key="'.esc_attr($key).'"><i class="fa fa-trash"></i></button>'
-        );
-        ?>
-    </div>
-<? } ?>
-</div>
 
 <?php
 $this->partial('settings/get-bug-modal', array());
