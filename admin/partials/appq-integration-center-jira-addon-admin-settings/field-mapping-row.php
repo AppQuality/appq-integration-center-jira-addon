d93ace13ff99f5fd2a0505a6124c9edad9efbcc2
Davide Bizzi <davide.bizzi@app-quality.com>
2021-03-22 16:05:58 +0100

    feat: Make edit/delete button flat

--- admin/partials/appq-integration-center-jira-addon-admin-settings/field-mapping-row.php
+++ admin/partials/appq-integration-center-jira-addon-admin-settings/field-mapping-row.php
@@ -9,14 +9,14 @@
 							<?= $is_json_icon ?>
 						</div>
 						<div class="col-2 text-right actions">
-								<button data-toggle="modal" data-target="#add_mapping_field_modal" type="button" class="btn btn-info btn-sm mr-1 edit-mapping-field"
+								<button data-toggle="modal" data-target="#add_mapping_field_modal" type="button" class="btn btn-info btn-icon-toggle mr-1 edit-mapping-field"
 												data-key="<?= $_key; ?>"
 												data-content="<?= (isset($item['value']) ? ($item['value']) : '') ?>"
 												data-sanitize="<?= (isset($item['sanitize']) ? ($item['sanitize']) : '') ?>"
 												data-json="<?= (isset($item['is_json']) ? ($item['is_json']) : '') ?>">
 										<i class="fa fa-pencil"></i>
 								</button>
-								<button data-toggle="modal" data-target="#delete_mapping_field_modal" type="button" class="btn btn-danger btn-sm delete-mapping-field"
+								<button data-toggle="modal" data-target="#delete_mapping_field_modal" type="button" class="btn btn-danger btn-icon-toggle delete-mapping-field"
 												data-key="<?= $_key ?>">
 										<i class="fa fa-trash"></i>
 								</button>
