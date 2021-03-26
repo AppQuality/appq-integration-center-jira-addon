a61779b9f36210ce1624ad09e4d0ef0ec548de4b
Davide Bizzi <davide.bizzi@app-quality.com>
2021-03-26 16:53:45 +0100

    fix: Remove specific credentials modal

--- admin/partials/appq-integration-center-jira-addon-admin-settings/current-setup.php
+++ admin/partials/appq-integration-center-jira-addon-admin-settings/current-setup.php
@@ -59,20 +59,3 @@ $project  = isset( json_decode( $campaign->bugtracker->endpoint )->project ) ? j
       ?>
     </div>
 </div>
-
-<!-- Modal -->
-<div class="modal fade" id="apiKeyModal" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Authentication</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <pre><code><?= $apikey; ?></code></pre>
-            </div>
-        </div>
-    </div>
-</div>
